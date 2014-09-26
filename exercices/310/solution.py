c059e0e9e729f917416703f05f8c82065e6b2b99
Camille Blakeley <cblakeleydejy@gmail.com>
2014-09-26 11:00:04 +0200

    other

--- exercices/310/solution.py
+++ exercices/310/solution.py
@@ -4,4 +4,10 @@ Created on Thu Sep 25 16:13:14 2014
 
 @author: sblakeley
 """
-f=open('words')
+f = open('words')
+for i in f :
+    total=0
+    if i = 'e'
+    total = total+1
+    else:
+    total = total
