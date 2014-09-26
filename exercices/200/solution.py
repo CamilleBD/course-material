744e0db1023b86cde08aeabe9399475477dfd7a8
Camille Blakeley <cblakeleydejy@gmail.com>
2014-09-26 12:59:14 +0200

    add

--- exercices/200/solution.py
+++ exercices/200/solution.py
@@ -7,10 +7,13 @@ Created on Wed Sep 24 12:28:15 2014
 
 
 def is_prime(num):
-    for i in range(2, num):
-        if num == 1 or num == 2:
+    for i in range(0, num):
+        if num == 2:
             return True
+        if num == 1:
+            return False
         if num % i == 0:
             return False
     else:
+        print(True)
         return True
