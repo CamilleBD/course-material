57a01884d234ac6de7c012a30ceee117ecf5a643
Camille Blakeley <cblakeleydejy@gmail.com>
2014-09-26 12:54:01 +0200

    add

--- exercices/110/solution.py
+++ exercices/110/solution.py
@@ -4,26 +4,22 @@ Created on Tue Sep 23 14:56:36 2014
 
 @author: sblakeley
 """
-import sys 
-inp=(sys.argv)
+import sys
+inp = (sys.argv)
 for i, arg in enumerate(sys.argv):
-    if arg=="+":
-        ans=float(inp[1])+float(inp[3])
+    if arg == "+":
+        ans = float(inp[1]) + float(inp[3])
         print(ans)
-    elif arg=="-":
-        ans=float(inp[1])-float(inp[3])
+    elif arg == "-":
+        ans = float(inp[1]) - float(inp[3])
         print(ans)
-    elif arg=="*":
-        ans=float(inp[1])*float(inp[3])
+    elif arg == "*":
+        ans = float(inp[1]) * float(inp[3])
         print(ans)
-    elif len(inp)==1:
-        print('a_number','(an_operator +-*\%^)', 'a_number')
-    elif sys.argv[3]=="0" and arg=="/":
+    elif len(inp) == 1:
+        print('a_number', '(an_operator +-*\%^)', 'a_number')
+    elif sys.argv[3] == "0" and arg == "/":
         print("input error")
-    elif arg=="/":
-        ans=float(inp[1])/float(inp[3])
+    elif arg == "/":
+        ans = float(inp[1]) / float(inp[3])
         print(ans)
-
-        
-    
-    
